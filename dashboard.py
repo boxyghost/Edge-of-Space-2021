1c2b570cf50222625b313d8b542a10b0fcee2c2c
Demeaus Wong <54847296+demwong@users.noreply.github.com>
2021-07-20 11:10:34 -0600

    Added dashboard.py file

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+"""
+@breif Software for displaying all collected data from EOSS flight in one dashboard
+
+@author Demeaus Wong <------ (Please add your name here if you work on this file!)
+@python version 3.8.2
+@date 7/20/21
+@bugs None yet!
+
+@TODO create basic dashboard, read in csv data files, display sensor data in charts, create 3D representation of flight data, display video
+
+"""
+
+import dash
+import dash_core_components as dcc
+
+print("Hello")
