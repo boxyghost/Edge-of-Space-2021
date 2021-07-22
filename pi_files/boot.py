7acd6c89f84b5006c829f7ebccc6c38f5c3bda64
Demeaus Wong <54847296+demwong@users.noreply.github.com>
2021-07-22 12:50:33 -0600

    Removed references to second arduino port

--- pi_files/boot.py
+++ pi_files/boot.py
@@ -13,8 +13,7 @@ time_stamp = strftime('%Y-%m-%d-%H_%M_%S', gmtime())
 arduino_port0 = '/dev/ttyACM0' # Port for the first Teensy
 arduino_port1 = '/dev/ttyACM1' # Port for the second Teensy
 baud = 9600 #i leave this
-fileName = "/home/pi/Edge_of_Space/datalogger_ACM0_" + time_stamp + ".csv" #name of the file you are going to write to
-fileName2 = "/home/pi/Edge_of_Space/datalogger_ACM1_" + time_stamp + ".csv" # File for the second Arduino port
+fileName = "/home/pi/Edge-of-Space-2021/datalogger_ACM0_" + time_stamp + ".csv" #name of the file you are going to write to
 
 print_label = False
 #you don't need to touch the rest
@@ -22,15 +21,12 @@ result = None
 while result is None:
     try:
         result = serial.Serial(arduino_port0, baud)
-        result = serial.Serial(arduino_port1, baud)
     except:
         pass
     
 
 ser_0 = serial.Serial(arduino_port0, baud)
-ser_1 = serial.Serial(arduino_port1,baud)
-print("Attempting to connecting to Arduino with Port:  " + arduino_port0)
-print("Attempting to connect to Arduino with Port:  " + arduino_port1)
+print("Attempting to connect to Arduino with Port:  " + arduino_port0)
 
 
 while True:
@@ -42,16 +38,8 @@ while True:
     with open(fileName, "a") as file_0:
         time_stamp = strftime('%Y-%m-%d-%H_%M_%S', gmtime())
         file_0.write(time_stamp + "," + data + "\n")
-    
-    getData=str(ser_1.readline())
-    data=getData[0:][:-2]
-    print(data)
-    
-    with open(fileName2, "a") as file_1:
-        time_stamp = strftime('%Y-%m-%d-%H_%M_%S', gmtime())
-        file_1.write(time_stamp + "," + data + "\n")
-
         
+    ############ See hackrf_auto.py ##################
     # Use hackrf_transfer to log data collected by the HackRF
     #op_freq_1 = str(100000000) # 100 MHz
     #op_freq_2 = str(400000000) # 400 MHz
